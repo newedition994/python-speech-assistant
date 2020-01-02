1479874107f7a89df4c1092bad2b70af30a9d10f
Breyanna Vass <breyannavass@gmail.com>
2020-01-02 14:17:59 -0500

    changed from print to grant speak

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ r = sr.Recognizer()
 def record_audio(ask=False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            grant_speak(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            print('Sorry, I did not get that')
+            grant_speak('Sorry, I did not get that')
         except sr.RequestError:
-            print('Sorry, my speech service is down')
+            grant_speak('Sorry, my speech service is down')
         return voice_data
 
 
@@ -37,25 +37,25 @@ def grant_speak(audio_string):
 
 def respond(voice_data):
     if 'what is your name' in voice_data:
-        print('My name is Grant')
+        grant_speak('My name is Grant')
     if 'what time is it' in voice_data:
-        print(ctime())
+        grant_speak(ctime())
     if 'search' in voice_data:
         search = record_audio('What do you want search')
         url = 'http://google.com/search?q=' + search
         webbrowser.get().open(url)
-        print('Here is what I found for ' + search)
+        grant_speak('Here is what I found for ' + search)
     if 'find location' in voice_data:
         location = record_audio('What is the location')
         url = 'http://google.nl/maps/place/' + location + '/&amp;'
         webbrowser.get().open(url)
-        print('Here is what I found for ' + location)
+        grant_speak('Here is what I found for ' + location)
     if 'exit' in voice_data:
         exit()
 
 
 time.sleep(1)
-print('How can I help you?')
+grant_speak('How can I help you?')
 while 1:
     voice_data = record_audio()
     respond(voice_data)

de20478a9b63cae5252e4f2f9b9f8c95e1bf33f8
Breyanna Vass <breyannavass@gmail.com>
2020-01-02 13:53:36 -0500

    made record audio and respond function

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('Say something')
-    audio = r.listen(source)
-    try:
-        voice_data = r.recognize_google(audio)
-        print(voice_data)
-    except sr.UnknownValueError:
-        print('Sorry, I did not get that')
-    except sr.RequestError:
-        print('Sorry, my speech service is down')
+
+def record_audio():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print('Sorry, I did not get that')
+        except sr.RequestError:
+            print('Sorry, my speech service is down')
+        return voice_data
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print('My name is Grant')
+
+
+print('How can I help you?')
+voice_data = record_audio()
+print(voice_data)
+respond(voice_data)

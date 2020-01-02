b34156a57d35eea0d3d8d959b8da254d8f54f0eb
Breyanna Vass <breyannavass@gmail.com>
2020-01-02 13:47:40 -0500

    adding exceptions to application

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     print('Say something')
     audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
+    try:
+        voice_data = r.recognize_google(audio)
+        print(voice_data)
+    except sr.UnknownValueError:
+        print('Sorry, I did not get that')
+    except sr.RequestError:
+        print('Sorry, my speech service is down')
